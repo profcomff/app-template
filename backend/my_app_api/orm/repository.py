6a8aefe02483a739d4003e9a2bfb05d45a6cf5e6
Алексей Уханов <dropdatabase.py@gmail.com>
2024-04-20 16:42:33 +0300

    Add reposytory class

---
+++ backend/my_app_api/orm/repository.py
@@ -0,0 +1,10 @@
+from sqlalchemy.dialects.postgresql import array
+from fastapi import HTTPException
+from sqlalchemy import delete, or_, select
+
+from my_app_api.models.models_db import PostOrm
+# from ..schemas.banner_schemas import *
+
+
+class BannerRepository:
+    pass
